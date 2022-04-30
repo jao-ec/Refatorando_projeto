2cbd1eba7c21c4e7e84ab292853dbacb7247869c
João Matheus <70983676+jao-ec@users.noreply.github.com>
2022-04-30 02:18:50 -0300

    Update Community.java

--- refatorando/Community.java
+++ refatorando/Community.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Community {
 
     Scanner input = new Scanner(System.in);
+    Search searcher = new Search();
 
     ArrayList<Friend>   members     = new ArrayList<Friend>();
     ArrayList<Post>     newsletters = new ArrayList<Post>();
@@ -14,7 +15,7 @@ public class Community {
     private String theme_community;
     private String about_community;
     String res;
-    boolean active, flag;
+    boolean active, flag, member;
     int choise, choise2;
 
     public Community(String self)
@@ -35,101 +36,197 @@ public class Community {
                "Post amount: "    +newsletters.size()  +"\n";
     }
 
-    public void menuCommunity(Profile self)
+    public void menuCommunity(Profile self, ArrayList<Profile> accounts, ArrayList<Community> community)
     {
         active = true;
-        while(active)
-        {
-            
-            System.out.printf(this.showCommunity()+"\n[1] - Entry\n[2] - Leave the community\n[3] - Members\n"+
-                                                   "[4] - Newsletters\n[5] - Exit\n=>");
-            choise = input.nextInt();
-            input.nextLine();
+        member = false;
+        String noMember = "\n[1] - Entry\n[2] - Members\n[3] - Newsletters\n[4] - Exit\n=>";
+        String Member = "\n[1] - Leave the community\n[2] - Members\n[3] - Newsletters\n[4] - Exit\n=>";
+        String adminMember = "\n[1] - Members\n[2] - Messages\n[3] - Newsletters\n[4] - Delete community\n[5] - Exit\n=>";
 
-            switch(choise)
+        for(int i=0; i<this.members.size(); i++)
+        {
+            if(this.members.get(i).getUsername().intern() == self.getUser().intern())
             {
-                case 1:
-                    this.members.add(new Friend(self.getUser()));
-                    System.out.println("\nWelcome to the "+getCommunity_name()+" community");
-                    break;
-                ////////////////////////////////////////////////////////////////////////////////////////////
-                case 2:
-                    flag = false;
-
-                    for(int i=0; i<this.members.size(); i++)
-                    {
-                        if(this.members.get(i).getUsername().intern() == self.getUser().intern())
-                        {
-                            this.members.remove(i);
-                            
-                            for(int j=0; j<self.communities.size(); i++)
-                            {
-                                if(self.communities.get(j).getCommunity_name().intern() == this.getCommunity_name().intern())
-                                {
-                                    self.communities.remove(j);
-                                    break;
-                                }
-                            }
-
-                            System.out.printf("\nSuccessful exit from "+getCommunity_name()+" community\n");
-                            flag = true;
-                        }
-                    }
-
-                    if(!flag)
-                    {
-                        System.out.println("\nYou must be in the community to be able to leave.\n");
-                    }
-                    break;
-                //////////////////////////////////////////////////////////////////////////////////////////////
-                case 3:
-                    this.showMembers();
-                    break;
-                ///////////////////////////////////////////////////////////////////////////////////////////////
-                case 4:
-                    System.out.printf("\n[1] - Make a post\n[2] - See Posts\n[3] - Exit\n=>");
-                    choise2 = input.nextInt();
-                    input.nextLine();
-
-                    switch(choise2)
-                    {
-                        case 1:
-                            Post post = new Post(self);
-                            this.newsletters.add(post);
-                            break;
-                        case 2:
-                            this.posts(self);
-                            break;
-                        case 3:
-                            break;
-                    }
-                    break;
-                //////////////////////////////////////////////////////////////////////////////////////////////
-                case 5:
-                    active = false;
-                    break;
-            }
-        }
-
-    }
-
-    public void posts(Profile self)
-    {
-        boolean member = false;
-
-        if(this.newsletters.isEmpty())
-        {
-            System.out.println("\nThis community does not have any post");
-            return;
-        }
-
-        for(int i=0; i<members.size(); i++)
-        {
-            if(members.get(i).getUsername().intern() == self.getUser().intern())
-            {
-                member = true;
+                this.member = true;
                 break;
             }
+        }
+
+        while(active)
+        {
+            if(this.getUser_admin().intern() == self.getUser().intern())
+            {
+                System.out.printf(this.showCommunity()+adminMember);
+                this.choise = input.nextInt();
+                input.nextLine();
+                active = this.adminMenu(self, accounts, community);
+            }else
+            {
+                if(member)
+                {
+                    System.out.printf(this.showCommunity()+Member);
+                    this.choise = input.nextInt();
+                    input.nextLine();
+                    active = this.memberMenu(self);
+                }
+                else
+                {
+                    System.out.printf(this.showCommunity()+noMember);
+                    this.choise = input.nextInt();
+                    input.nextLine();
+                    active = this.noMemberMenu(self);
+                }
+            }
+        }
+    }
+
+    private boolean noMemberMenu(Profile self)
+    {
+        switch(this.choise)
+        {
+            case 1:
+                    members.add(new Friend(self.getUser()));
+                    self.communities.add(this);
+                    this.member = true;
+                    System.out.println("\n"+self.getUser()+" welcome to the "+getCommunity_name()+" community");
+                    input.nextLine();
+                    break;
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            case 2:
+                this.showMembers();
+                break;
+            ///////////////////////////////////////////////////////////////////////////////////////////
+            case 3:
+            System.out.printf("\n[1] - See Posts\n[2] - Exit\n=>");
+            choise2 = input.nextInt();
+            input.nextLine();
+
+            switch(choise2)
+            {
+                 case 1:
+                    this.posts(self, false);
+                    break;
+                case 2:
+                    break;
+            }
+            break;
+            //////////////////////////////////////////////////////////////////////////////////////////////
+            case 4:
+                return false;
+        }
+        return true;
+    }
+
+    private boolean memberMenu(Profile self)
+    {
+        switch(this.choise)
+        {
+            case 1:
+                for(int i=0; i<this.members.size(); i++)
+                {
+                    if(this.members.get(i).getUsername().intern() == self.getUser().intern())
+                    {
+                        this.members.remove(i);
+                        this.member = false;
+                        
+                        for(int j=0; j<self.communities.size(); i++)
+                        {
+                            if(self.communities.get(j).getCommunity_name().intern() == this.getCommunity_name().intern())
+                            {
+                                self.communities.remove(j);
+                                break;
+                            }
+                        }
+                        System.out.printf("\nSuccessful exit from "+getCommunity_name()+" community\n");
+                        input.nextLine();
+                        break;
+                    }
+                }
+                break;
+            //////////////////////////////////////////////////////////////////////////////////////////////
+            case 2:
+                this.showMembers();
+                break;
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+            case 3:
+                System.out.printf("\n[1] - Make a post\n[2] - See Posts\n[3] - Exit\n=>");
+                choise2 = input.nextInt();
+                input.nextLine();
+
+                switch(choise2)
+                {
+                    case 1:
+                        Post post = new Post(self);
+                        this.newsletters.add(post);
+                        System.out.printf("\nPost successfully published.\n");
+                        input.nextLine();
+                        break;
+                    case 2:
+                        this.posts(self, true);
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+            //////////////////////////////////////////////////////////////////////////////////////////////
+            case 4:
+                return false;
+        }
+        return true;
+    }
+
+
+    private boolean adminMenu(Profile self, ArrayList<Profile> accounts, ArrayList<Community> community)
+    {
+        switch(this.choise)
+        {
+            case 1:
+                this.showMembers();
+                break;
+            ////////////////////////////////////////
+            case 2:
+                this.showMessages();
+                break;
+            //////////////////////////////////////
+            case 3:
+            System.out.printf("\n[1] - Make a post\n[2] - See Posts\n[3] - Exit\n=>");
+            choise2 = input.nextInt();
+            input.nextLine();
+
+            switch(choise2)
+            {
+                case 1:
+                    Post post = new Post(self);
+                    System.out.printf("\nPost successfully published.\n");
+                    input.nextLine();
+                    this.newsletters.add(post);
+                    break;
+                case 2:
+                    this.posts(self, member);
+                    break;
+            }
+            break;
+            ///////////////////////////////////////////
+            case 4:
+                self.admin_communities.remove(this);
+                community.remove(this);
+                System.out.printf("\nCommunity successfully deleted.\n");
+                input.nextLine();
+                return this.delet(accounts);
+            case 5:
+                return false;
+        }
+        return true;
+    }
+
+    public void posts(Profile self, boolean member)
+    {
+        if(this.newsletters.isEmpty())
+        {
+            System.out.printf("\nThis community does not have any post\n");
+            input.nextLine();
+            return;
         }
 
         for(int i=0; i<this.newsletters.size(); i++)
@@ -148,6 +245,19 @@ public class Community {
                 }
             }
         }
+    }
+
+    public boolean delet(ArrayList<Profile> accounts)
+    {  
+        this.newsletters.clear();
+        this.messages.clear();
+
+        for(int i=0; i<this.members.size(); i++)
+        {
+            accounts.get(searcher.search(accounts, this.members.get(i).getUsername())).communities.remove(this);
+        }
+        this.members.clear();
+        return false;
     }
 
     public void showMembers()
